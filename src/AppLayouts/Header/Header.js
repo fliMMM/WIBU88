702919969ca48d31bb2eb167f00d23c7ecb50cea
bui huy bach <bachkame@gmail.com>
2022-03-20 01:36:05 +0700

    fix logo

--- src/AppLayouts/Header/Header.js
+++ src/AppLayouts/Header/Header.js
@@ -1,30 +1,44 @@
 import React from "react";
 import { Nav, NavLink, Bars, NavMenu, NavBtnLink } from "./style";
-import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 const Header = () => {
   const user = false;
   return (
-    
     <>
       <Nav>
         <NavLink to="/">
           <img
-            style={{ height: "50px" }}
-            src={require("../../image/logo.png")}
+            style={{ height: "50px", borderRadius: "10px" }}
+            src={require("../../image/logo.gif")}
+            alt="logo"
+          />
+          <img
+            style={{
+              height: "50px",
+              borderRadius: "10px",
+              transform: "scale(0.80)",
+              marginLeft: "-17px",
+            }}
+            src={require("../../image/logo1.png")}
             alt="logo"
           />
         </NavLink>
         <Bars />
         <NavMenu>
           <NavLink to="/cart" activestyle="true">
-            <ShoppingCartOutlinedIcon style={{marginRight: "5px"}}/>Giỏ Hàng
+            <ShoppingCartOutlinedIcon style={{ marginRight: "5px" }} />
+            Giỏ Hàng
           </NavLink>
-          {user && <NavLink to="/user-profile" activestyle="true">
-            Vua Lì đòn
-          </NavLink>}
-          {!user && <NavLink to="/sign-up" activestyle="true">
-            Đăng Ký
-          </NavLink>}
+          {user && (
+            <NavLink to="/user-profile" activestyle="true">
+              Vua Lì đòn
+            </NavLink>
+          )}
+          {!user && (
+            <NavLink to="/sign-up" activestyle="true">
+              Đăng Ký
+            </NavLink>
+          )}
           {!user && <NavBtnLink to="/sign-in">Đăng Nhập</NavBtnLink>}
         </NavMenu>
       </Nav>
