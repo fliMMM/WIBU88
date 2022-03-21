695f8fe6ab95768f6c70776360859df82cf68c0c
bui huy bach <bachkame@gmail.com>
2022-03-21 22:56:52 +0700

    test

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -5,7 +5,7 @@ function Products(){
         <div style={{paddingTop: "50px"}}>
             
         <h1 >Đây là trang Products</h1>
-        <h1 >Chào Đức</h1>
+        <h1 >Chào Đức</h1><h1 >Chào Hiếu</h1>
         </div>
     )
 }
